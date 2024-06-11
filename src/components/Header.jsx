fbd1f1da81b0d4e8c335afa3e35d560ce94455ff
Agustin Braco <bracoagustin@gmail.com>
2024-06-11 14:32:20 -0300

    [Dev] - Remove scroll handler from Header for styles issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,9 @@ import { Logo, MenuMobile, MenuDesktop } from '.';
 const viewport = 412;
 const isMenuMobile = viewport < 915;
 
-export const Header = () => {
-  const [isScrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () =>
-      window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <header id="Header" className={`${isScrolled ? 'White' : 'Transparent'}`}>
-      <Logo />
-      {isMenuMobile ? <MenuMobile /> : <MenuDesktop />}
-    </header>
-  );
-};
+export const Header = () => (
+  <header id="Header">
+    <Logo />
+    {isMenuMobile ? <MenuMobile /> : <MenuDesktop />}
+  </header>
+);
